49399ac75a0e940264dda2c63c802e2276c06641
arifweb2020 <arifhussainb2019@gmail.com>
2021-06-25 19:24:52 +0530

    second way form submitted

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,8 +1,25 @@
 <template>
     <div>
-        <input type="text"  v-model="loginForm.username" placeholder="enter name" /><br>
+        <!-- <input type="text"  v-model="loginForm.username" placeholder="enter name" /><br>
+        <input type="password"  v-model="loginForm.password" placeholder="enter password"/>
+       <br> <button v-on:click="submit">Login</button> -->
+<p >
+    <b>correct the following errors</b>
+    <ul>
+        <li v-for="e in loginForm.error" v-bind:key="e.id">
+            {{e}}
+            </li>
+        </ul>
+
+
+    </p>
+
+       <form @submit="login">
+
+ <input type="text"  v-model="loginForm.username" placeholder="enter name" /><br>
         <input type="password"  v-model="loginForm.password" placeholder="enter password"/>
        <br> <button v-on:click="submit">Login</button>
+           </form>
         
         </div>
 </template>
@@ -11,9 +28,27 @@
 export default {
     name:'Form',
 
-    methods: {
-        submit(){
+    // methods: {
+    //     submit(){
+    //         console.log("login data", this.loginForm.username , this.loginForm.password)
+
+    //     }
+    // }
+     methods: {
+         login(e)
+        {
+            this.loginForm.error=[];
+            if(this.loginForm.username && this.loginForm.password){
+                console.log("no error")
+            }
+            if(!this.loginForm.username){
+                this.error.push("username is required")
+            }
+            if(!this.loginForm.password){
+                this.error.push("password is required")
+            }
             console.log("login data", this.loginForm.username , this.loginForm.password)
+            e.preventDefault();
 
         }
     },
@@ -23,6 +58,7 @@ export default {
         return{
             loginForm: 
             {
+                error:null,
                 username:null,
                 password:null
             }
